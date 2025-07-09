68ce211ff523f3d50b61b465ae25e008c352e8ad
Miyuki5hiba <112722600+Miyuki5hiba@users.noreply.github.com>
2025-07-09 11:52:37 -0400

    Delete java/com/allergen_info_service directory

--- java/com/allergen_info_service/Repositorys/FoodRepository.java
+++
@@ -1,8 +0,0 @@
-package com.allergen_info_service.Repositorys;
-
-import com.allergen_info_service.Models.Food;
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface FoodRepository extends CrudRepository<Food, Long>{
-}
